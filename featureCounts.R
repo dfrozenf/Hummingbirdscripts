5eda950d0bdb68e5de36909ecb38fd90513b5e5c
dfrozenf <46003913+dfrozenf@users.noreply.github.com>
2019-01-27 05:28:02 -0800

    Update featureCounts.R

--- featureCounts.R
+++ featureCounts.R
@@ -5,7 +5,7 @@ files=
 		c("~/RNAseq/STARmap/STAR66Aligned.out.bam",
 		"~/RNAseq/STARmap/STAR73Aligned.out.bam",
 		"~/RNAseq/STARmap/STAR78Aligned.out.bam",
-		"~/RNAseq/STARmap/STAR81Aligned.out.bam")
+		"~/RNAseq/STARmap/STAR81Aligned.out.bam"),
 annot.ext="~/Genomes/Celegans/PRJNA13758/c_elegans.PRJNA13758.WS253.canonical_geneset.gtf",
 isGTFAnnotationFile=TRUE,
 GTF.featureType="exon",
